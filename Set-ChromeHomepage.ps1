83484256348dcb2d07009c5e35e033e7f2306381
cmdlette <90802834+cmdlette@users.noreply.github.com>
2021-09-18 22:32:55 -0500

    Create Set-ChromeHomepage.ps1

---
+++ Set-ChromeHomepage.ps1
@@ -0,0 +1,15 @@
+# Set registry path variables
+$chromerec = "HKLM:\SOFTWARE\Policies\Google\Chrome\Recommended"
+$chromeurl = "HKLM:\SOFTWARE\Policies\Google\Chrome\Recommended\RestoreOnStartupURLs"
+
+# Create necessary registry paths
+New-Item $chromerec -Force
+New-Item $chromeurl -Force
+
+# Set site to open on Chrome startup and set as homepage
+# This section also enables the home button, which users seem to appreciate!
+Get-Item -Path $chromerec | Set-ItemProperty -Name "RestoreOnStartup" -Value 4
+Get-Item -Path $chromeurl | Set-ItemProperty -Name "1" -Value "https://domain.tld"
+Get-Item -Path $chromerec | Set-ItemProperty -Name "ShowHomeButton" -Value 1
+Get-Item -Path $chromerec | Set-ItemProperty -Name "HomepageLocation" -Value "https://domain.tld"
+Get-Item -Path $chromerec | Set-ItemProperty -Name "HomepageIsNewTabPage" -Value 0
